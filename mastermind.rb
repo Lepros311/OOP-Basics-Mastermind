08f05daf3ccf572bf734e51560d825702370f380
Lepros311 <65778034+Lepros311@users.noreply.github.com>
2024-03-21 20:02:41 -0400

    Add ability for player to choose role.

--- mastermind.rb
+++ mastermind.rb
@@ -3,27 +3,34 @@ class Player
   @@secret_code = []
   @@possible_colors = ["red", "orange", "green", "yellow", "white", "black"]
 
-  attr_accessor :name, :secret_code, :guess, :possible_colors
+  attr_accessor :name, :secret_code, :guess, :possible_colors, :role
 
-  def initialize(name)
+  def initialize(name, role)
     @name = name
+    @role = role
   end
 
   def codemaker_turn
-    puts "\n#{self.name}, let's make your secret code!"
-    4.times do |i|
-      loop do
-        puts "Color #{i+1} - red, orange, green, yellow, white, or black?"
-        @@secret_code[i] = gets.chomp.downcase
-        if @@possible_colors.include?(@@secret_code[i])
-          break
-        else
-          @@secret_code.pop
-          puts "That is not a valid option. Please try again."
+    if self.name == "Computer"
+      4.times do
+        @@secret_code.push(@@possible_colors.sample)
+      end
+    else
+      puts "\n#{self.name}, let's make your secret code!"
+      4.times do |i|
+        loop do
+          puts "Color #{i+1} - red, orange, green, yellow, white, or black?"
+          @@secret_code[i] = gets.chomp.downcase
+          if @@possible_colors.include?(@@secret_code[i])
+            break
+          else
+            @@secret_code.pop
+            puts "That is not a valid option. Please try again."
+          end
         end
       end
+      puts "#{self.name}, your secret code is #{@@secret_code}"
     end
-    puts "#{self.name}, your secret code is #{@@secret_code}"
   end
 
   def codebreaker_turn
@@ -81,25 +88,44 @@ end
 class Game
   puts "~Mastermind~"
 
-  puts "\nCodemaker, what's your name?"
-  codemaker = Player.new(gets.chomp)
+  puts "\nPlayer 1, what's your name?"
+  p1name = gets.chomp
 
   for i in 1..Float::INFINITY
-    puts "\n#{codemaker.name}, are you playing against the computer?"
+    puts "\n#{p1name}, what role do you want - codemaker or codebreaker?"
+    p1role = gets.chomp.downcase
+    if ["codemaker", "codebreaker"].include?(p1role)
+      break
+    else
+      puts "\nInvalid response. Please answer 'codemaker' or 'codebreaker'..."
+    end
+  end
+
+  player1 = Player.new(p1name, p1role)
+  p2role = p1role == "codemaker" ? "codebreaker" : "codemaker"
+
+  for i in 1..Float::INFINITY
+    puts "\nAre you playing against the computer?"
     comp = gets.chomp.downcase
     if ["no", "n"].include?(comp)
-      puts "\nCodebreaker, what's your name?"
-      codebreaker = Player.new(gets.chomp)
+      puts "\nPlayer 2, what's your name?"
+      p2name = gets.chomp
+      player2 = Player.new(p2name, p2role)
     elsif ["yes", "y"].include?(comp)
-      codebreaker = Player.new("Computer")
+      player2 = Player.new("Computer", p2role)
     else
       puts "\nInvalid response. Please answer yes or no..."
     end
     break if ["no", "n", "yes", "y"].include?(comp)
   end
 
-  codemaker.codemaker_turn
-  codebreaker.codebreaker_turn
+  if player1.role == "codemaker"
+    player1.codemaker_turn
+    player2.codebreaker_turn
+  else
+    player2.codemaker_turn
+    player1.codebreaker_turn
+  end
 end
 
 Game.new
